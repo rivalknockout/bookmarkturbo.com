b9d2b1a66ce4ce17f44a3f024d79b95083282a2d
hideaki <rivalknockout@gmail.com>
2013-03-19 16:53:22 +0900

    createThemeGround.jsとcreateParent.jsをcreate.jsにまとめた

--- js/create.js
+++ js/create.js
@@ -10,6 +10,14 @@ function createThemeGround(stack_name, stack_id)
 }
 
 
+function createParent(stack_id, stack_name, booksArray)
+{
+	stack_id = stack_id || 'null';
+	
+	var $HIGHEST = $('#stack_id' +stack_id+ '.theme-ground  .base-rail');
+	$HIGHEST.append( makeParentHTML(booksArray) );
+}
+
 
 //----------------------------------------------------------------------
 //	Primary functions
@@ -35,6 +43,33 @@ function makeThemeGroundHTML(stack_name, stack_id)
 }
 
 
+function makeParentHTML(booksArray)
+{
+	var HTML = '';
+	
+	
+	HTML+='<div class="books parent">';
+	
+	for( i in booksArray )
+	{
+		var book_name	= booksArray[i].name;
+		var book_id		= booksArray[i].id;
+		HTML+='<div data-bookId="' +book_id+ '"></div>';
+		HTML+='\	<p>' +book_name+ '</p>';
+		HTML+='</div>';
+	}
+	
+	HTML+='</div>';
+	
+	
+	return HTML;
+}
+
+
 //----------------------------------------------------------------------
 //	Tiny functions
 //----------------------------------------------------------------------
+
+
+
+
